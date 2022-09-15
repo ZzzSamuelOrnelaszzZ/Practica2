671c7d991e67a5d824d1a282aebaa2fab65aeb69
itsdo <itsdo@DESKTOP-42QB0C4>
2022-09-14 17:26:59 -0700

    Agregar archivos de proyecto.

---
+++ Practica2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int edad = 21;
+            string nombre = "Samuel";
+            string pais = "México";
+
+            Console.WriteLine("MI EDAD ES: {0} LUEGO MI NOMBRE ES {1} Y FINALMENTE SOY DE {2}", edad, nombre, pais);
+            Console.ReadLine();
+
+        }
+
+    
+
+
+
+        
+    }
+}
